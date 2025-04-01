79e1c6687d9e2ad48658b82a2dae9dc7cb933737
Brian-NN <brianndungu274@gmail.com>
2025-04-01 05:57:29 +0300

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -16,8 +16,6 @@
      <link rel="stylesheet" href="bootstrap/css/bootstrap-reboot.css.map">
      <link rel="stylesheet" href="bootstrap/css/bootstrap-reboot.min.css">
      <link rel="stylesheet" href="bootstrap/css/bootstrap-reboot.min.css.map">
-   
-     <link rel="stylesheet" href="css/e-commerce.css">
 
     <script src="bootstrap/js/bootstrap.bundle.js"></script>
     <script src="bootstrap/js/bootstrap.bundle.js.map"></script>
